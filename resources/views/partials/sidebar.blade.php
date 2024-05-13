dc212af02242f44c3446180921abe8d034987c26
unknown <indomiesotosidoarjo@gmail.com>
2024-05-13 15:46:24 +0700

    grammar materi sudah

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -110,5 +110,11 @@ class="layout-menu menu-vertical menu bg-menu-theme"
       <div data-i18n="Soal">Soal</div>
     </a>
   </li>
+  <li class="menu-item">
+    <a href="/materiGrammar" class="menu-link">
+      <i class="menu-icon tf-icons ti ti-notes"></i>
+      <div data-i18n="Materi">materi</div>
+    </a>
+  </li>
 </ul>
 </aside>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UjianController;
 use App\Http\Controllers\SoalController;
+use App\Http\Controllers\MateriGrammarController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -43,6 +44,15 @@ Route::middleware('auth')->group(function() {
         Route::put('/soal/{code}', 'update')->name('soal.update');
         Route::delete('/soal/{code}', 'destroy')->name('soal.destroy');
     });
+
+    Route::controller(MateriGrammarController::class)->group(function() {
+        Route::get('/materiGrammar', 'index')->name('materiGrammar.index');
+         Route::get('/materiGrammar/create', 'create')->name('materiGrammar.create');
+         Route::get('/materiGrammar/{code}', 'edit')->name('materiGrammar.edit');
+         Route::post('/materiGrammar', 'store')->name('materiGrammar.store');
+         Route::put('/materiGrammar/{code}', 'update')->name('materiGrammar.update');
+         Route::delete('/materiGrammar/{code}', 'destroy')->name('materiGrammar.destroy');
+     });
 });
 
 Route::post('/log', [AuthController::class, 'auth'])->name('auth');
