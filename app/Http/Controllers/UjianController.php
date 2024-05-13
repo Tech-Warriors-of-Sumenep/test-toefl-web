a3d38e2253ef681afe5864b221d384e0280b77b7
muhammadalief19 <aliefmuhammad1908@gmail.com>
2024-05-13 07:39:30 +0700

    feat(Ujian): added feature CRUD Ujian

--- app/Http/Controllers/UjianController.php
+++ app/Http/Controllers/UjianController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Ujian;
 use Illuminate\Http\Request;
 
 class UjianController extends Controller
@@ -11,7 +13,11 @@ class UjianController extends Controller
      */
     public function index()
     {
-        return view('ujian.home');
+        $ujian = Ujian::with(['category'])->get();
+        $no = 1;
+        return view('ujian.index', compact([
+            'ujian', 'no'
+        ]));
     }
 
     /**
@@ -19,7 +25,10 @@ class UjianController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('ujian.create', compact([
+            'categories'
+        ]));
     }
 
     /**
@@ -27,7 +36,26 @@ class UjianController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate($request, [
+            'title' => 'required|min:8',
+            'category' => 'required',
+            'deskripsi' => 'required|min:8'
+        ], [
+            'title.required' => 'title harus terisi',
+            'title.min' => 'panjang title minimal 8 karakter',
+            'category.required' => 'kategori harus terisi',
+            'deskripsi.required' => 'deskripsi harus terisi',
+            'deskripsi.min' => 'panjang deskripsi minimal 8 karakter'
+        ]);
+
+        Ujian::create([
+            'uuid' => uniqid(),
+            'title' => $request->title,
+            'description' => $request->deskripsi,
+            'category_id' => $request->category
+        ]);
+
+        return redirect()->route('ujian.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 
     /**
@@ -41,24 +69,54 @@ class UjianController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $code)
     {
-        //
+        $categories = Category::all();
+        $ujian = Ujian::with(['category'])->where('uuid', $code)->first();
+        return view('ujian.edit', compact(
+            [
+                'categories', 'ujian'
+            ]
+        ));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $code)
     {
-        //
+        $ujian = Ujian::with(['category'])->where('uuid', $code)->first();
+
+        $this->validate($request, [
+            'title' => 'required|min:8',
+            'category' => 'required',
+            'deskripsi' => 'required|min:8'
+        ], [
+            'title.required' => 'title harus terisi',
+            'title.min' => 'panjang title minimal 8 karakter',
+            'category.required' => 'kategori harus terisi',
+            'deskripsi.required' => 'deskripsi harus terisi',
+            'deskripsi.min' => 'panjang deskripsi minimal 8 karakter'
+        ]);
+
+        $ujian->update([
+            'title' => $request->title,
+            'description' => $request->deskripsi,
+            'category_id' => $request->category
+        ]);
+
+        return redirect()->route('ujian.index')->with(['success' => 'Data Berhasil Diupdate!']);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $code)
     {
-        //
+        $ujian = Ujian::with(['category'])->where('uuid', $code)->first();
+        
+        $ujian->delete();
+
+        return redirect()->route('ujian.index')->with(['success' => 'Data Berhasil Didelete!']);
     }
 }
